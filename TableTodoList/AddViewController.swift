78c4b0d3a8e373efde961de4bdffea7fdb53b15e
uracle <sodud@uracle.co.kr>
2024-01-05 17:29:50 +0900

    스케줄 프로젝트 생성

---
+++ TableTodoList/AddViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddViewController.swift
+//  TableTodoList
+//
+//  Created by 유라클 on 2024/01/05.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet var tfAddItem: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnAddItem(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
